9b4d684a0bc7b0903687ac8af629c1400dbb5fe7
eLoopWoo <eyzenberg.tomer@gmail.com>
2017-07-28 14:57:28 +0300

    initial commit main

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+
+from machine import machine
+from pprint import pprint
+
+def main():
+    me = machine()
+    neighbors_info = me.show_network_ip_mac_addresses()
+    diks_info = me.list_disk_partitions()
+    pprint(neighbors_info)
+    print "-"*80
+    pprint(diks_info)
+
+if __name__ == '__main__':
+    main()
